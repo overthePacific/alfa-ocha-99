f340faf11312bfec192371fe071a63337364927b
overthePacific <89888785+overthePacific@users.noreply.github.com>
2021-09-15 10:15:52 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'さいご',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'さいし',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'にちようび',
           // q2: 'かかか',
           // q3: 'ききき',
         }
@@ -100,7 +100,7 @@ app.component('answer-input', {
     return {
       /* 送信ボタン上下に表示されるメッセージ */
       okMessage: '正解！',
-      ngMessage: 'そのキーワードは違うようだぞ！？',
+      ngMessage: 'なんか違うような…？',
       message: '',
       inputAnswer: '',
     }
